d00fa339f121f76e66355bb58a6cd5035a733530
Lucas-dev-br <yulucasgx2@hotmail.com>
2023-01-06 14:52:10 -0300

    Rolagem de tela

--- src/components/Participant/index.tsx
+++ src/components/Participant/index.tsx
@@ -1,10 +1,21 @@
-import { View, Text } from "react-native";
+import { View, Text, TouchableOpacity } from "react-native";
+import { styles } from "./styles";
 
-export function Participant() {
+type Props = {
+  name: string;
+  onRemove: () => void;
+};
+
+export function Participant({ name, onRemove }: Props) {
   return (
     <View>
-      <View>
-        <Text>Lucas Henrique</Text>
+      <View style={styles.container}>
+        <Text style={styles.name}>{name}</Text>
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText} onPress={onRemove}>
+            -
+          </Text>
+        </TouchableOpacity>
       </View>
     </View>
   );
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -5,18 +5,26 @@ import {
   Touchable,
   TouchableOpacity,
 } from "react-native";
+
 import { styles } from "./styles";
 
+import { Participant } from "../../components/Participant";
+
 export function Home() {
+  const partcipants = ["Lucas", "Vinicius", "Karla", "Rodrigo"];
+
   function handleParticipantAdd() {
-    console.log("Teste");
+    console.log("Adicionar");
+  }
+
+  function handleParticipantRemove(name: string) {
+    console.log(`Você clickou para remover o candidato ${name}`);
   }
 
   return (
     <View style={styles.container}>
       <Text style={styles.eventName}>Nome do evento</Text>
       <Text style={styles.eventDate}>Sexta, 3 de agosto</Text>
-
       <View style={styles.form}>
         <TextInput
           style={styles.input}
@@ -27,6 +35,10 @@ export function Home() {
           <Text style={styles.buttonText}>+</Text>
         </TouchableOpacity>
       </View>
+      <Participant
+        name="Lucas"
+        onRemove={() => handleParticipantRemove("Lucas")}
+      />
     </View>
   );
 }
