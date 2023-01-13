1a0c51be0d2894789583bf59127ea56042a989c1
Lucas-dev-br <yulucasgx2@hotmail.com>
2023-01-13 11:23:29 -0300

    project complete

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -15,24 +15,29 @@ import { styles } from "./styles";
 import { Participant } from "../../components/Participant";
 
 export function Home() {
-  const [participants, setParticipants] = useState(["João"]);
+  const [participants, setParticipants] = useState<string[]>([]);
+  const [participantName, setParticipantName] = useState("");
 
   function handleParticipantAdd() {
-    if (participants.includes("Rodrigo")) {
+    if (participants.includes(participantName)) {
       return Alert.alert(
         "Participante Existe",
         "Já existe um participante na lista"
       );
     }
 
-    setParticipants((prevState) => [...prevState, "Ana"]);
+    setParticipants((prevState) => [...prevState, participantName]);
+    setParticipantName("");
   }
 
   function handleParticipantRemove(name: string) {
     Alert.alert("Remover", `Remover o participante ${name}?`, [
       {
         text: "Sim",
-        onPress: () => Alert.alert("Deletado!"),
+        onPress: () =>
+          setParticipants((prevState) =>
+            prevState.filter((participant) => participant !== name)
+          ),
       },
       {
         text: "Não",
@@ -43,13 +48,15 @@ export function Home() {
 
   return (
     <View style={styles.container}>
-      <Text style={styles.eventName}>Nome do evento</Text>
-      <Text style={styles.eventDate}>Sexta, 3 de agosto</Text>
+      <Text style={styles.eventName}>Nome dos candidatos</Text>
+      <Text style={styles.eventDate}>Criado por Lucas Henrique</Text>
       <View style={styles.form}>
         <TextInput
           style={styles.input}
           placeholder="Nome do candidato"
           placeholderTextColor="#a1a1a1"
+          onChangeText={setParticipantName}
+          value={participantName}
         />
         <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
           <Text style={styles.buttonText}>+</Text>
